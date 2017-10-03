e5c7975dbaf6ef7889c5018b43adbba150bd4381
resema <5762933+resema@users.noreply.github.com>
2017-10-03 20:49:31 +0200

    vectorized impl started

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,10 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = 1/m * sum(-y * log(sigmoid(X * theta)) - (1-y) * log(1 - sigmoid(X * theta))) ...
+    + lambda / (2*m) * sum(theta(2:end)' * theta(2:end));
 
-
-
-
-
-
-
-
+grad = 
 
 % =============================================================
 
